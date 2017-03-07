28b8030302a46d6e8bf1d6cf113d54e2975cab02
zubati <jaranin@hotmail.com>
2017-03-07 16:52:04 +0700

    First check in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,40 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AF } from './providers/af';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {RouterModule, Routes} from "@angular/router";
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyCyUY9GMUlYI1RQHmA3hHJuBR8yEIuR8DQ",
+  authDomain: "fir-crud-57aaf.firebaseapp.com",
+  databaseURL: "https://fir-crud-57aaf.firebaseio.com",
+  storageBucket: "fir-crud-57aaf.appspot.com",
+  messagingSenderId: "612904572355"
+};
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AF],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
